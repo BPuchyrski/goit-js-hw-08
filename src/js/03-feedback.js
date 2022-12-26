2f8673c3345f11eb622a9d2398b06edb6d69f494
BPuchyrski <bartosz.puchyrski@gmail.com>
2022-12-26 13:46:46 +0100

    u

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,24 @@
+let throttle = require('lodash.throttle');
+let form = document.querySelector('.feedback-form');
+let mail = form.querySelector('input');
+let textArea = form.querySelector('textarea');
 
+let toLocalStorage = () => {
+  let store = {
+    email: mail.value,
+    message: textArea.value,
+  };
+  localStorage.setItem('feedback-form-state', JSON.stringify(store));
+};
+
+form.addEventListener('input', throttle(toLocalStorage, 500));
+
+let storedOutput = JSON.parse(localStorage.getItem('feedback-form-state'));
+
+if (localStorage.getItem('feedback-form-state') === null) {
+  inputEmail.value = '';
+  inputText.value = '';
+} else {
+  mail.value = storedOutput.email;
+  textArea.value = storedOutput.message;
+}
